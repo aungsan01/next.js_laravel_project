c09939c82a93bb4a89124dd0d5d700be5813090c
AUNG SAN LIN <148869020+aungsan01@users.noreply.github.com>
2025-03-11 16:17:58 +0700

    commit about home.jsx

--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -1,13 +1,96 @@
+"use client";
 // @flow strict
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React, { useState } from 'react';
 
-import * as React from 'react';
+const Menu = () => {
+    const pathname = usePathname();
+    console.log(pathname);
+    const [fixedHeader] = useState(false);
 
-function Menu() {
     return (
-        <div>
-            Menu
-        </div>
+        <>
+            <div className={`header-menu bg-white ${fixedHeader ? 'fixed' : ''}`}>
+                <div className="container flex items-center justify-between h-20">
+                    <Link className='menu-left-block' href="/">
+                        <Image
+                            src={'/images/logo.png'}
+                            width={2000}
+                            height={1000}
+                            alt='logo'
+                            priority={true}
+                            className='w-[149px] max-sm:w-[132px]'
+                        />
+                    </Link>
+
+                    <div className="menu-block h-full">
+
+                        <ul className='menu-nav flex items-center xl:gap-2 h-full'>
+
+                            <li className={`nav-item h-full flex items-center justify-center home ${pathname === '/' ? 'active' : ''} mx-4`}>
+
+                                <Link className='nav-link text-title flex items-center gap-1 text-blue-500' href="/">
+                                    <span> Home
+                                    </span>
+                                </Link>
+                            </li>
+
+
+                            <li className={`nav-item h-full flex items-center justify-center home ${pathname === '/about' ? 'active' : ''} mx-4`}>
+
+                                <Link className='nav-link text-title flex items-center gap-1 text-blue-500' href="/about">
+                                    <span> About Us
+                                    </span>
+                                </Link>
+                            </li>
+
+                            <li className={`nav-item h-full flex items-center justify-center home ${pathname === '/service' ? 'active' : ''} mx-4`}>
+
+                                <Link className='nav-link text-title flex items-center gap-1 text-blue-500' href="/service">
+                                    <span> Services
+                                    </span>
+                                </Link>
+                            </li>
+
+                            <li className={`nav-item h-full flex items-center justify-center home ${pathname === '/casestudy' ? 'active' : ''} mx-4`}>
+
+                                <Link className='nav-link text-title flex items-center gap-1 text-blue-500' href="/casestudy">
+                                    <span> Case Study
+                                    </span>
+                                </Link>
+                            </li>
+
+                            <li className={`nav-item h-full flex items-center justify-center home ${pathname === '/contact' ? 'active' : ''} mx-4`}>
+
+                                <Link className='nav-link text-title flex items-center gap-1 text-blue-500' href="/contact">
+                                    <span> Contact
+                                    </span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                    <div className = 'menu-right-block flex items-center'>
+                        <div className= 'icon-call'>
+                    <i className = 'icon-phone-call text-4xl;'></i>
+
+                        </div>
+
+                    <div className ='text ml-3'>
+
+                        <div classNmae = 'text caption1'> Contact to me </div>
+                        <div classNmae = 'number text-button'> +6550238756 </div>
+                    </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </>
     );
-};
+}
 
 export default Menu;
--- src/components/Header/TopNav/TopNav.jsx
+++ src/components/Header/TopNav/TopNav.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import * as Icon from '@phosphor-icons/react/dist/ssr'
+import * as Icon from '@phosphor-icons/react/dist/ssr';
+import Link from 'next/link';
 const TopNav = () => {
     return (
         <>
@@ -7,12 +8,39 @@ const TopNav = () => {
                 <div className='container flex items-center justify-between h-[44px]'>
                     <div className='left-block flex items-center'>
                         <div className='location flex items-center max-lg:hidden'>
-                      <Icon.MapPin className = 'text-white text-xl'/>
-                        <span className= 'ml-2 caption1 text-white'>  LetterPark ,Bangkok </span>
+                            <Icon.MapPin className='text-white text-xl' />
+                            <span className='ml-2 caption1 text-white'>  LetterPark ,Bangkok </span>
                         </div>
+
+
+                        <div className='mail lg:ml-7 flex items-center'>
+                            <Icon.Envelope className='text-white text-xl' />
+                            <span className='ml-2 caption1 text-white'>  aungsanlin1414@gmail.com  </span>
+                        </div>
+                    </div>
+
+                    <div className='right-block flex items-center gap-5 '>
+                        <div className='line h-6 w-px bg-grey max-sm:hidden'></div>
+                        <div className='list-social flex items-center gap-2.5 max-sm:hidden '></div>
+
+                        <Link className='item rounded-full w-7 h-7 border-gray-50 border-2 flex items-center justify-center' href="https://facebook.com/" target='_blank'>
+                            <Icon.FacebookLogo className='text-white text-sm' />
+                        </Link>
+
+                        <Link className='item rounded-full w-7 h-7 border-gray-50 border-2 flex items-center justify-center' href="https://www.linkedin.com/in/aung-san-lin-a670ba29b/" target='_blank'>
+                            <Icon.LinkedinLogo className='text-white text-sm' />
+                        </Link>
+
+                        <Link className='item rounded-full w-7 h-7 border-gray-50 border-2 flex items-center justify-center' href="https://github.com/aungsan01" target='_blank'>
+                            <Icon.GithubLogo className='text-white text-sm' />
+                        </Link>
+
                     </div>
                 </div>
             </div>
+
+
+
         </>
     );
 };
